c266fe4a2396d53f51760d445d0042767743d751
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2019-06-10 22:21:05 +0900

    顔を複数検出する

--- static/index.js
+++ static/index.js
@@ -3,6 +3,7 @@ const app = new Vue({
   delimiters: ['[[', ']]'],
   data: {
     predictions: [],
+    canvases: [],
     loading: true,
   },
   mounted: function() {
@@ -38,7 +39,7 @@ const app = new Vue({
       reader.readAsDataURL(files[0]);
     },
     downloadCanvasImage: function() {
-      const canvas = document.querySelector('#mainCanvas')
+      const canvas = document.querySelector('.mainCanvas')
       const link = document.createElement("a");
       link.href = canvas.toDataURL("image/png");
       link.download = parseInt((new Date()).getTime()/1000) + "_0_0.png";
@@ -47,76 +48,75 @@ const app = new Vue({
     predict: function(imageFile = './static/sample.jpg') {
       const vm = this
       vm.loading = true
-      setupCanvas(imageFile, 128, 128).then((resp) => {
-        if (resp !== false) {
-          const b64 = resp.canvas.toDataURL("image/png").replace(/data:image\/png;base64,/, '');
-          const formData = new FormData();
-          formData.append("image", b64);
-
-          window.superagent
-            .post('/')
-            .send(formData)
-            .then(res => {
-              const results = []
-              const data = res.body.data
-              for (let i = 0; i < data.length; i++) {
-                results.push([data[i][1], parseFloat(data[i][0]).toFixed(2)])
-              }
-              vm.predictions = results.sort((a, b) => {
-                return b[1] - a[1]
-              })
-            });
-          vm.loading = false
+      vm.setupCanvas(imageFile, 128, 128).then((canvases) => {
+        if (canvases !== false) {
+          for (let i = 0; i < canvases.length; i++) {
+            const canvas = canvases[i]
+            const b64 = canvas.toDataURL("image/png").replace(/data:image\/png;base64,/, '');
+            const formData = new FormData();
+            formData.append("image", b64);
+  
+            window.superagent
+              .post('/')
+              .send(formData)
+              .then(res => {
+                const results = []
+                const data = res.body.data
+                for (let i = 0; i < data.length; i++) {
+                  results.push([data[i][1], parseFloat(data[i][0]).toFixed(2)])
+                }
+                vm.predictions = results.sort((a, b) => {
+                  return b[1] - a[1]
+                })
+              });
+            vm.loading = false
+          }
         } else {
           alert("顔が検出できませんでした");
           vm.predictions = [];
           vm.loading = false
         }
       })
+    },
+    setupCanvas: function(imgUrl, resizeW, resizeH) {
+      const vm = this
+      return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.crossOrigin = "Anonymous";
+        img.src = imgUrl;
+        img.onload = async () => {
+          const detections = await vm.faceDetect(img)
+          vm.canvases = detections
+          if (!detections) {
+            resolve(false)
+          } else {
+            setTimeout(() => {
+              const canvases = document.querySelectorAll('.mainCanvas')
+              for (let i = 0; i < canvases.length; i ++) {
+                const detection = detections[i]
+                const canvas = canvases[i];
+                if (!canvas) return null;
+                const context = canvas.getContext("2d");
+                if (!context) return null;
+                canvas.width = resizeW;
+                canvas.height = resizeH;
+                const squareW = detection.box.width > detection.box.height ? detection.box.width : detection.box.height
+                const squareH = detection.box.width > detection.box.height ? detection.box.width : detection.box.height
+                context.drawImage(img, detection.box.x, detection.box.y, squareW, squareH, 0, 0, resizeW, resizeH);
+              }
+              resolve(canvases);
+            }, 100)
+          }
+        };
+        img.onerror = () => reject(null);
+      });
+    },
+    faceDetect: async function(canvas) {
+      await faceapi.nets.ssdMobilenetv1.loadFromUri('./static')
+      const options = new faceapi.SsdMobilenetv1Options({ minConfidence: 0.5 })
+      const detections = await faceapi.detectAllFaces(canvas, options)
+      return detections
     }
   }
 });
 
-async function faceDetect(canvas) {
-  await faceapi.nets.ssdMobilenetv1.loadFromUri('./static')
-  const options = new faceapi.SsdMobilenetv1Options({ minConfidence: 0.5 })
-  const detections = await faceapi.detectSingleFace(canvas, options) 
-  return detections
-}
-
-function setupCanvas(imgUrl, resizeW, resizeH) {
-  const canvas = document.querySelector('#mainCanvas');
-  if (!canvas) return null;
-  const context = canvas.getContext("2d");
-  if (!context) return null;
-  return new Promise((resolve, reject) => {
-    const img = new Image();
-    img.crossOrigin = "Anonymous";
-    img.src = imgUrl;
-    img.onload = async () => {
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      const detections = await faceDetect(img)
-      if (!detections) {
-        resolve(false)
-      } else {
-        canvas.width = resizeW;
-        canvas.height = resizeH;
-        const squareW = detections.box.width > detections.box.height ? detections.box.width : detections.box.height
-        const squareH = detections.box.width > detections.box.height ? detections.box.width : detections.box.height
-        context.drawImage(img, detections.box.x, detections.box.y, squareW, squareH, 0, 0, resizeW, resizeH);
-        const imgData = context.getImageData(0, 0, canvas.width, canvas.height);
-        console.log(detections.box.x, detections.box.y, detections.box.width, detections.box.height)
-        console.log(detections.box.x, detections.box.y, squareW, squareH)
-        resolve({
-          canvas: canvas,
-          context: context,
-          imgData: imgData,
-          pixels: imgData.data,
-          width: canvas.width,
-          height: canvas.height
-        });
-      }
-    };
-    img.onerror = () => reject(null);
-  });
-}
